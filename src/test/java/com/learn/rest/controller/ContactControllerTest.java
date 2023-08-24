0502a9ff35719067b7068a1d88aaa269217fbcb9
mgshaikh <139044368+mgshaikh@users.noreply.github.com>
2023-08-24 20:02:08 +0530

    add env variable

--- src/test/java/com/learn/rest/controller/ContactControllerTest.java
+++ src/test/java/com/learn/rest/controller/ContactControllerTest.java
@@ -1,26 +1,22 @@
 package com.learn.rest.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.learn.rest.exception.ContactNotFoundException;
-import com.learn.rest.model.Contact;
-import com.learn.rest.respository.ContactRepository;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.*;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.learn.rest.model.Contact;
+import com.learn.rest.respository.ContactRepository;
 
 @WebMvcTest(ContactController.class)
 public class ContactControllerTest {
